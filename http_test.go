78e6ac945c4ec07de5989934a40510a15b4b349a
simsimler <sslr.mc@gmail.com>
2021-09-11 02:15:16 +0900

    feat(test): add tests

--- http_test.go
+++ http_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestHTTPClient_GetBot(t *testing.T) {
+	client := NewHTTPClient()
+	bot, err := client.GetBot("770246143652397069")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%s, (%d Servers/%d Votes)\n", bot.Name, bot.Servers, bot.Votes)
+}
+
+func TestHTTPClient_GetUser(t *testing.T) {
+	client := NewHTTPClient()
+	user, err := client.GetUser("441202161481809922")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%s, (%d Bots)\n", user.Username, len(user.Bots))
+}
+
 func TestHTTPClient_SearchBots(t *testing.T) {
 	client := NewHTTPClient()
 	bots, err := client.SearchBots("도박", 1)
@@ -12,7 +30,7 @@ func TestHTTPClient_SearchBots(t *testing.T) {
 		t.Error(err)
 	}
 	for idx, bot := range bots.Data {
-		fmt.Printf("%d. %s, (%d Servers/%d Votes)\n", idx+1, bot.Name, bot.Servers, bot.Servers)
+		fmt.Printf("%d. %s, (%d Servers/%d Votes)\n", idx+1, bot.Name, bot.Servers, bot.Votes)
 	}
 }
 
